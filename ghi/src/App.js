62512674e951b36e8b91d8914a264c028d9651f3
Tristen Martinez <tristen.m1324@gmail.com>
2023-01-04 11:46:19 -0700

    added navbar componenet to build out once I get the puiblic backend served with kubernetes

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import ChatRoom from './component/ChatRoom'
+import NavBar from './component/NavBar'
 const App = () => {
   return (
+    <>
+    <NavBar/>
     <ChatRoom/>
+    </>
   )
 }
 
